e5f4805c3f90a222d4a54617a1584f1e8181a45b
WorkSerge <cncserge@gmail.com>
2023-04-02 10:46:55 +0300

    add doc

---
+++ src/global.h
@@ -0,0 +1,10 @@
+#ifndef _GLOBAL_H_
+#define _GLOBAL_H_
+
+void mainPage(void);
+
+
+
+
+
+#endif // _GLOBAL_H_
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
+#include <U8g2lib.h>
+#include "global.h"
+U8G2_ST7920_128X64_F_HW_SPI lcd (U8G2_R0, 5, 22);
+
+
+
 
 void setup() {
-  // put your setup code here, to run once:
+  lcd.setBusClock(500000); 
+  lcd.begin();
+  lcd.enableUTF8Print();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  lcd.firstPage();
+
+  do {
+    mainPage();
+  }while ( lcd.nextPage() );
+}
+
+
+void mainPage(void){
+
 }
